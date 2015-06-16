7520e816124eb907a38fbe3578233e2e716d1831
sempf1992 <stefanfranssen1992@hotmail.com>
2015-06-16 09:48:26 +0000

    Fix bij een paar calls

--- EC.py
+++ EC.py
@@ -12,7 +12,7 @@ class Curve:
     #initialiseer een curve
     #gooit een exceptie als hij singulier is
     def __init__(self, Prime, alpha, beta):
-        
+        self.Prime = Prime
         self.a = FiniteFieldElem(Prime,alpha)
         self.b = FiniteFieldElem(beta)
         
@@ -26,18 +26,19 @@ class Curve:
         return -16*(4 * self.a ** 3 + 27 * self.b ** 2)
     
     def TestPunt(self, x, y):
-        return y*y == x * x * x + self.a * x + b
+        return y*y == x * x * x + self.a * x + self.b
     #functie die je een nieuw punt aan laat maken
-    def NieuwPunt(self):
-        return Punt(self)
+    def NieuwPunt(self,x,y):
+        return Punt(self, FiniteFieldElem(self.Prime, x), FiniteFieldElem(self.Prime, y))
         
 class Punt:
     def __init__(self, curve, x_, y_):
         self.curve = curve
-        #maak x en y de identiteit
+        #maak punten x en y aan
         self.x = x_
         self.y = y_
-        if 
+        if not curve.TestPunt(self.x,self.y):
+           raise ValueError('Point is not a point on the curve')
         return
     
     #schrijf operatoren
