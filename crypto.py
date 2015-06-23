e8ec14158d46448a141340cdb000646ac08dfe38
sempf1992 <stefanfranssen1992@Hotmail.com>
2015-06-23 11:56:47 +0200

    Update crypto.py

--- crypto.py
+++ crypto.py
@@ -1,67 +1,75 @@
-from stef import AES
-
+from AES_good import AES
 
 def texttobytes(text):
     bytes = []
     i = 0
-    while (i < len(text)):
+    while ( i < len(text)):
         bytes.append(ord(text[i]))
         i += 1
     return bytes
 
-
 def bytestotext(bytes):
     text = ""
     i = 0
-    while (i < len(bytes)):
+    while ( i < len(bytes)):
         text = text + chr(bytes[i])
         i += 1
     return text
-
-
+        
 class Crypto:
     def Crypto(self):
-        # do the initialisation
+        #do the initialisation
         return
-
+        
     def KeyAgreement(self):
-        # do a Diffey-Helman key exchange
-        key = texttobytes('h?2Trq]k8$s;H,D+')
+        #do a Diffey-Helman key exchange
+        key =  texttobytes('h?2Trq]k8$s;H,D+')
         iv = texttobytes('h?2Trq]k8$s;H,D+')
         self.AES = AES(key, iv)
         return
-
+    
     def Encrypt(self, plaintext):
         plaintextbytes = texttobytes(plaintext)
         ciphertextbytes = self.AES.encrypt(plaintextbytes)
         return ciphertextbytes
-
+        
     def Decrypt(self, ciphertextbytes):
         plaintextbytes = self.AES.decrypt(ciphertextbytes)
         plaintext = bytestotext(plaintextbytes)
         return plaintext
-
+        
     def selftest(self):
-
+        
         teststring = "teststring"
 
-        # set key and iv
-        key = texttobytes('h?2Trq]k8$s;H,D+')
+        #set key and iv
+        key =  texttobytes('h?2Trq]k8$s;H,D+')
         iv = texttobytes('h?2Trq]k8$s;H,D+')
-
-        # do a selftest
+        
+        #do a selftest
         self.AES = AES(key, iv)
-        # self.AES.selftest()
-
-        # encrypt
-        self.AES = AES(key, iv)
-        outstring = self.Encrypt(teststring)
-
-        # decrypt
-        # self.AES = AES(key, iv)
-        # plaintext = self.Decrypt(outstring)
-
-        # print(plaintext)
+        self.AES.selftest()
+        teststring2 = teststring
+        run = 1
+        
+        while ( run < 100):
+            teststring2 = teststring2 + teststring
+            #encrypt
+            self.AES = AES(key, iv)
+            outstring = self.Encrypt(teststring2)
+        
+            #decrypt
+            self.AES = AES(key, iv)
+            plaintext = self.Decrypt(outstring)
+            
+            if (plaintext != teststring2):
+                print("fout by run = " + str(run))
+                print(teststring2)
+                print(plaintext)
+                
+            run += 1
+        
+        print("klaar")
 
 crypt = Crypto()
 crypt.selftest()
