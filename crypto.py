181cc021aed1f4b920df1b2e4befc82560aa2746
sempf1992 <stefanfranssen1992@Hotmail.com>
2015-06-30 20:19:14 +0200

    Update crypto.py

--- crypto.py
+++ crypto.py
@@ -1,6 +1,18 @@
 from AES_good import AES
 from EC import *
 import random
+import hmac
+import hashlib
+
+def compare_digest(x, y):
+    if not (isinstance(x, bytes) and isinstance(y, bytes)):
+        raise TypeError("both inputs should be instances of bytes")
+    if len(x) != len(y):
+        return False
+    result = 0
+    for a, b in zip(x, y):
+        result |= a ^ b
+    return result == 0
 
 def texttobytes(text):
     bytes = []
@@ -20,80 +32,133 @@ def bytestotext(bytes):
         
 class LSEC: #Locally Stored Elliptic Curve
 		#using curve P-521, source: http://cs.ucsb.edu/~koc/ccs130h/notes/ecdsa-cert.pdf page 42 
-		p = 1
-		a = 0
-		b = 0
-		xG = 0 #xG,yG finite field elements
-		yG = 0
-		G = Punt(xG,yG)
+		p = 23
+		a = 2
+		b = 1
+		n = 15
+		curve = Curve(p,a,b)
+		xG = FiniteFieldElem(p,10) #xG,yG finite field elements
+		yG = FiniteFieldElem(p,3) #xG,yG finite field elements
+		G = Punt(curve,xG,yG)
+		##Method for finding points
+		##for x in range(1, 22):
+		##	xG = FiniteFieldElem(p,x) #xG,yG finite field elements
+		##	for y in range(1, 22):
+		##		try:
+		##			yG = FiniteFieldElem(p,3)
+		##			G = Punt(curve,xG,yG)
+		##			print(G)
+		##			break
+		##		except:
+		##			continue
+
 class Crypto:
-    def Crypto(self):
-        #do the initialisation
-        return
+	def Crypto(self):
+		#do the initialisation
+		return
         
-    def KeyAgreement(self):
-        #do a Diffie-Helman key exchange	
-        key =  texttobytes('h?2Trq]k8$s;H,D+')
-        iv = texttobytes('h?2Trq]k8$s;H,D+')
-        self.AES = AES(key, iv)
-        return
+	def KeyGen(self, KeyData):
+		#do a Diffie-Helman key exchange	
+		key =  texttobytes('h?2Trq]k8$s;H,D+')
+		key2 = bytes('h?2Trq]k8$s;H,D+', 'utf-8')
+		iv = texttobytes('h?2Trq]k8$s;H,D+')
+		self.AES_enc = AES(key, iv)
+		self.AES_dec = AES(key, iv)
+		self.enc_hmac = hmac.new(key2)#, "", hashlib.sha256)
+		self.dec_hmac = hmac.new(key2)#, "", hashlib.sha256)
+		return
     
-	def DHSendHost(self, target):
-	    a = random.randint(5014,7234)
-		return LSEC.G*a #send this to target
+	def DHSendHost(self):
+		self.a = random.randint(1, LSEC.n)
+		return str(LSEC.G.Clone()*self.a) #send this to target
 
 	def DHRecHost(self, UGmessage):
-		return UGmessage*a #shared secret for host
+		if UGmessage == "id":
+			RecPoint = Eenheid(LSEC.curve)
+		else:
+			prime = LSEC.p
+			x_ = int(UGmessage.split()[1])
+			x = FiniteFieldElem(prime, x_)
+			y_ = int(UGmessage.split()[3])
+			y = FiniteFieldElem(prime, y_)
+			RecPoint = Punt(LSEC.curve, x,y)
+		self.KeyGen(RecPoint * self.a)
+		return
 		
-	def DHSendUser(self, host):
-		b = random.random(2012,4234)
-		return LSEC.G*b #send this to host
+	def DHSendUser(self):
+		self.b = random.randint(1,LSEC.n)
+		return str(LSEC.G.Clone()*self.b) #send this to host
 		
 	def DHRecUser(self, HGmessage):
-		return HGmessage*b #shared secret for user with host
+		if HGmessage == "id":
+			RecPoint = Eenheid(LSEC.curve)
+		else:
+			prime = LSEC.p
+			x_ = int(HGmessage.split()[1])
+			x = FiniteFieldElem(prime, x_)
+			y_ = int(HGmessage.split()[3])
+			y = FiniteFieldElem(prime, y_)
+			RecPoint = Punt(LSEC.curve, x,y)
+		self.KeyGen(RecPoint * self.b)
+		return 
 	
-    def Encrypt(self, plaintext):
-        plaintextbytes = texttobytes(plaintext)
-        ciphertextbytes = self.AES.encrypt(plaintextbytes)
-        return ciphertextbytes
+	def Encrypt(self, plaintext):
+		plaintextbytes = texttobytes(plaintext)
+		ciphertextbytes = self.AES_enc.encrypt(plaintextbytes)
+		ciphertext = bytestotext(ciphertextbytes)
+		#now put an hmac on the text
+		self.enc_hmac.update(bytes(ciphertext, 'utf-8'))
+		digest = self.enc_hmac.hexdigest()
+		ciphertext = ciphertext + str(digest)
+		return ciphertext
         
-    def Decrypt(self, ciphertextbytes):
-        plaintextbytes = self.AES.decrypt(ciphertextbytes)
-        plaintext = bytestotext(plaintextbytes)
-        return plaintext
+	def Decrypt(self, ciphertext):
+		#split the digest from the rest of the ciphertext
+		digest = ciphertext[len(ciphertext)-2*self.dec_hmac.digest_size:]
+		digest = bytes(digest, 'utf-8')
+		ciphertext = ciphertext[:len(ciphertext) - 2*self.dec_hmac.digest_size]	
+		#compare to the rest of the ciphertext
+		self.dec_hmac.update(bytes(ciphertext, 'utf-8'))
+		byteshex = bytes(self.dec_hmac.hexdigest(), 'utf-8')
+		if compare_digest(digest, byteshex):
+			#continue if correct		
+			ciphertextbytes = texttobytes(ciphertext)
+			plaintextbytes = self.AES_dec.decrypt(ciphertextbytes)
+			plaintext = bytestotext(plaintextbytes)
+			return plaintext
         
-    def selftest(self):
+	def selftest(self):
         
-        teststring = "teststring"
+		teststring = "teststring"
 
-        #set key and iv
-        key =  texttobytes('h?2Trq]k8$s;H,D+')
-        iv = texttobytes('h?2Trq]k8$s;H,D+')
+		#set key and iv
+		key =  texttobytes('h?2Trq]k8$s;H,D+')
+		iv = texttobytes('h?2Trq]k8$s;H,D+')
         
-        #do a selftest
-        self.AES = AES(key, iv)
-        self.AES.selftest()
-        teststring2 = teststring
-        run = 1
+		#do a selftest
+		self.AES = AES(key, iv)
+		self.AES.selftest()
+		teststring2 = teststring
+		run = 1
         
-        while ( run < 100):
-            teststring2 = teststring2 + teststring
-            #encrypt
-            self.AES = AES(key, iv)
-            outstring = self.Encrypt(teststring2)
-        
-            #decrypt
-            self.AES = AES(key, iv)
-            plaintext = self.Decrypt(outstring)
+		while ( run < 100):
+			teststring2 = teststring2 + teststring
+			#encrypt
+			self.AES = AES(key, iv)
+			outstring = self.Encrypt(teststring2)
+			
+			#decrypt
+			self.AES = AES(key, iv)
+			plaintext = self.Decrypt(outstring)
             
-            if (plaintext != teststring2):
-                print("fout by run = " + str(run))
-                print(teststring2)
-                print(plaintext)
+			if (plaintext != teststring2):
+				print("fout by run = " + str(run))
+				print(teststring2)
+				print(plaintext)
                 
-            run += 1
+			run += 1
         
-        print("klaar")
+		print("klaar")
 
-crypt = Crypto()
-crypt.selftest()
+#crypt = Crypto()
+#crypt.selftest()
